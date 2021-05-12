468741ca64b94477e5f03bc2fc65cc2f75f26c95
laiiihz <laiiihz@outlook.com>
2021-05-12 16:14:36 +0800

    add search api

--- lib/src/netease_music_api.dart
+++ lib/src/netease_music_api.dart
@@ -1,4 +1,5 @@
 part '_auth.dart';
+part '_search.dart';
 
 ///NeteaseMusicAPI alias
 const NeteaseMusicAPI NEM = const NeteaseMusicAPI();
@@ -6,4 +7,5 @@ const NeteaseMusicAPI NEM = const NeteaseMusicAPI();
 class NeteaseMusicAPI {
   const NeteaseMusicAPI();
   final _Auth auth = const _Auth();
+  final _Search search = const _Search();
 }
